855c7c2af592686bc479c82d9a3a24a76ae48e50
André Luiz Alvares <andrluiz.alvares.alvares84@gmail.com>
2020-06-09 22:21:46 -0300

    pulo e melhor acompanhamento dos estados
    a principio tentei por máquina de estados finita, mas percebi que seria
    ruim, pois quero que o personagem se mova mesmo em pulo.
    
    Uma bitset seria melhor, mas usar bool tá ok já

--- Assets/Scripts/ThirdPersonWalker.cs
+++ Assets/Scripts/ThirdPersonWalker.cs
@@ -6,12 +6,17 @@ public class ThirdPersonWalker : MonoBehaviour
 {
     public float velocidadeMov;
     public float deslocamentoAltura;
+    public float intensidadePulo;
     public LayerMask camadaChao;
     public Animator anim;
 
     Transform tr;
     Rigidbody rb;
     Transform trCam;
+
+    public bool estaNoChao;
+    public bool estaEmPulo;
+    public bool estaEmMovimento;
 
     void Awake() {
         tr = GetComponent<Transform>();
@@ -21,40 +26,71 @@ public class ThirdPersonWalker : MonoBehaviour
 
     void FixedUpdate() {
         // receber dados de entrada do jogador
+        bool apertouPulo = Input.GetButtonDown("Jump");
         float movH = Input.GetAxis("Horizontal");
         float movV = Input.GetAxis("Vertical");
 
         Vector3 mov = new Vector3(movH, 0, movV);
-
-        // rotacionar o jogador na direção do movimento
-        tr.LookAt(tr.position + trCam.TransformDirection(mov) * 5);
-
         if (mov.magnitude > 1f)
             mov.Normalize();
 
+        // detecta estados
+        RaycastHit chaoHit;
+        estaNoChao = Physics.Raycast(
+            tr.position,
+            Vector3.down,
+            out chaoHit,
+            deslocamentoAltura + 0.05f,
+            camadaChao
+        );
+
+        estaEmPulo = apertouPulo || !estaNoChao;
+        estaEmMovimento = mov.magnitude > 0.1f;
+
+        // pulo
+        rb.useGravity = estaEmPulo;
+        rb.constraints = (
+            RigidbodyConstraints.FreezeRotationX |
+            RigidbodyConstraints.FreezeRotationY |
+            RigidbodyConstraints.FreezeRotationZ
+        );
+        if (!estaEmPulo)
+            rb.constraints = rb.constraints | RigidbodyConstraints.FreezePositionY;
+
+        if (apertouPulo && estaNoChao) {
+            rb.AddForce(Vector3.up * intensidadePulo, ForceMode.Impulse);
+        }
+
+        // rotacionar o jogador na direção do movimento
+        if (estaEmMovimento)
+            tr.LookAt(tr.position + trCam.TransformDirection(mov) * 5);
+
         // andança do jogador
-        tr.Translate(0, 0, mov.magnitude * velocidadeMov * Time.deltaTime);
+        if (estaEmMovimento)
+            tr.Translate(0, 0, mov.magnitude * velocidadeMov * Time.deltaTime);
 
         // alimentando parâemtro anim
         anim.SetFloat("velocidade", mov.magnitude);
 
         // acompanhar chão
-        RaycastHit hit;
-        bool rcBateuNoChao = Physics.Raycast(
-            tr.position,
-            Vector3.down,
-            out hit,
-            Mathf.Infinity,
-            camadaChao
-        );
+        if (!estaEmPulo) {
+            RaycastHit hit;
+            bool rcBateuNoChao = Physics.Raycast(
+                tr.position,
+                Vector3.down,
+                out hit,
+                Mathf.Infinity,
+                camadaChao
+            );
 
-        if (rcBateuNoChao) {
-            Vector3 pos = tr.position;
-            pos.y = hit.point.y + deslocamentoAltura;
-            tr.position = pos;
+            if (rcBateuNoChao) {
+                Vector3 pos = tr.position;
+                pos.y = hit.point.y + deslocamentoAltura;
+                tr.position = pos;
+            }
+
+            // zerar inercia
+            rb.velocity = Vector3.zero;
         }
-
-        // zerar inercia
-        rb.velocity = Vector3.zero;
     }
 }
